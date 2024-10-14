17dbd4a05230a1dfecff5f62fa45656902477a47
Aditya021 <adityabinjagermath12@gmail.com>
2024-10-14 17:07:49 +0530

    Dyploying my all folders and files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>Hello,Everyone..!!</h3>
+      <h1>Welcome To My Website / ReactJs 🚀</h1>
+    </>
+  )
+}
+
+export default App
